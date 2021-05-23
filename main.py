92af91a0899cb2c2da07dc06fbdff0751b7064b3
Samuel Mayer Rufino <samrufino13@gmail.com>
2021-05-23 18:56:53 -0300

    desenhando o tabuleiro

--- main.py
+++ main.py
@@ -13,13 +13,37 @@ def carregarImagens():
     for peca in pecas:
         IMAGENS[peca] = pygame.transform.scale(pygame.image.load('img/' + peca + '.png'), (TAMANHO_CASA, TAMANHO_CASA))
 
+def desenhaTabuleiro(tela):
+    cores = [ pygame.Color(235, 235, 208), pygame.Color(119, 148, 85) ]
+    for linha in range(DIMENSAO):
+        for coluna in range(DIMENSAO):
+            cor = cores[ (linha + coluna) % 2 ]
+            pygame.draw.rect(tela, cor, pygame.Rect(coluna*TAMANHO_CASA, linha*TAMANHO_CASA, TAMANHO_CASA, TAMANHO_CASA))
+
+def desenhaPecas(tela, gEngine):
+    pass
+
+def desenhaEstadoAtual(tela, gEngine):
+    desenhaTabuleiro(tela)
+    desenhaPecas(tela, gEngine)
+
+
 def main():
     pygame.init()
     tela = pygame.display.set_mode(( LARGURA, ALTURA ))
     clock = pygame.time.Clock()
     tela.fill(pygame.Color('white'))
     gEngine = gameEngine.gameEngine()
-    print(gEngine.tabuleiro)
+    rodando = True
+    while rodando:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                rodando = False
+
+        desenhaEstadoAtual(tela, gEngine)
+        
+        clock.tick(MAX_FPS)
+        pygame.display.flip()   
 
 if __name__ == '__main__':
     main()
