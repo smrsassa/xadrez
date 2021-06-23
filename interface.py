b48884f9d68cab89c2f467a495a0fd01978245d3
Samuel Mayer <samrufino13@gmail.com>
2021-06-23 15:40:03 -0300

    função salvar as partidas

--- interface.py
+++ interface.py
@@ -1,5 +1,5 @@
 import pygame
-import webbrowser
+from webbrowser import open_new as openBrowserTab
 
 class Interface:
     def __init__(self, tela: pygame.Surface, tamTabuleiro: int) -> None:
@@ -10,19 +10,29 @@ class Interface:
 
     def clique(self, x: int, y: int) -> None:
         if y > 460 and y < 510:
-            self.botaoDownloadMovesOnClick()
+            pass
+            #self.botaoDownloadMovesOnClick()
         elif y > 510:
             self.botaoGitOnClick()
-
 
     def botaoDownloadMoves(self) -> None:
         downloadIcon = pygame.transform.scale(pygame.image.load('img/download.png'), (50, 50))
         self.tela.blit(downloadIcon, pygame.Rect(560, 460, 50, 50))
-    def botaoDownloadMovesOnClick(self):
-        pass
+    def botaoDownloadMovesOnClick(self, fen: str, movLog: list) -> None:
+        arquivo = open("partidas/partidas.txt", "a")
+
+        arquivo.write(fen)
+        arquivo.write("\n")
+        arquivo.writelines(mov + " - " for mov in movLog)
+        arquivo.write("\n")
+        arquivo.write("=-=" * 30)
+        arquivo.write("\n")
+
+        arquivo.close()
+
 
     def botaoGit(self) -> None:
         gitHubLogo = pygame.transform.scale(pygame.image.load('img/gitHub.png'), (50, 50))
         self.tela.blit(gitHubLogo, pygame.Rect(560, 510, 50, 50))
-    def botaoGitOnClick(self):
-        webbrowser.open_new('https://github.com/smrsassa/xadrez')
+    def botaoGitOnClick(self) -> None:
+        openBrowserTab('https://github.com/smrsassa/xadrez')
